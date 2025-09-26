8f63c56e7b0b5b207e72300ac7e1fb06522ae0f1
Your <your_email@example.com>
2025-09-26 12:46:35 +0800

    修改路由模式为HASH模式,避免dev环境刷新404

--- naes-console-admin-main/src/main.jsx
+++ naes-console-admin-main/src/main.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom"; // 切回 BrowserRouter
 import App from "./App";
 import "./index.css";
 import "./i18n";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
